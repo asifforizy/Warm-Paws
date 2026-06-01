dbc646f4fe6ab88f52f50323cd7cce05dac1058b
Asif Forizy <asifforizy@gmail.com>
2026-06-02 01:47:41 +0600

    card added

---
+++ src/Components/Card.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Card = ({ service }) => {
+    return (
+        <div>
+            <div key={service.id} className="card bg-base-100 shadow-xl p-5">
+                <figure>
+                    <img
+                        src={service.image}
+                        alt={service.serviceName}
+                        className="h-60 w-full object-cover"
+                    />
+                </figure>
+
+                <div className="card-body">
+                    <div><h2 className="card-title">
+                        {service.serviceName}
+                    </h2></div>
+
+                    <div>
+                        <p className="font-semibold text-lg text-primary">
+                            Price: ${service.price}
+                        </p>
+                    </div>
+                    <hr />
+
+                    <div className="flex justify-between ">
+                        <h1>Rating: </h1>
+                        <h1 className="border px-2 rounded">⭐ {service.rating}</h1>
+                    </div>
+
+
+
+                    <div className="card-actions ">
+                        <button className="btn btn-primary w-full hover:bg-base-100 hover:text-primary border-primary">
+                            View Details
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import Card from "../Components/Card";
 
 const Home = () => {
     const services = useLoaderData();
@@ -85,7 +86,19 @@ const Home = () => {
                 </Swiper>
             </section>
 
-           
+            <section className="my-16 lg:px-40 px-5">
+                <div>
+                    <h2 className="text-4xl font-bold text-center mb-10">
+                        Popular Winter Care Services
+                    </h2>
+
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                        {services?.map((service) => (
+                            <Card key={service.id} service={service} />
+                        ))}
+                    </div>
+                </div>
+            </section>
 
 
 
